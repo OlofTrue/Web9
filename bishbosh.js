6ea887698ceb82ec2a6fdf8ccc62e97e95f36cd4
OlofTrue <githube@true.se>
2020-04-22 10:31:43 +0200

    separate js+css

--- bishbosh.js
+++ bishbosh.js
@@ -1,19 +1,22 @@
 let runBishBosh = function (loop, div1, div2) {
-    let str; //,strRet='' 
+    let str, domParent = document.querySelector(".bishbosh_parent"),
+        domChildTmp = document.querySelector('template').content.cloneNode(true).firstElementChild
+        ,domChild;
+    domParent.innerHTML = '';
     for (let i = 1; i <= loop; i++) {
         str = ''
         if (i % div1 === 0) str = '-Bish'
         if (i % div2 === 0) str += '-Bosh'
-        str = (str.length) ? str.substr(1) : i;
-        //console.log(str); strRet+=str;
-        document.querySelector(".list-group").innerHTML += '<li class="list-inline-item list-group-item">' +
-            str + '</li>';
-    } // return strRet;
+        str = (str) ? str.substr(1) : i;
+        //console.log(str);
+            //domParent.innerHTML += '<div class="bishbosh p-2 border">' + str + '</div>'; 
+            //domParent.insertAdjacentHTML("beforeend",'<div class="bishbosh p-2 border">' + str + '</div>');
+        domChild=domChildTmp.cloneNode(true);
+        domChild.innerHTML = str;
+        domParent.appendChild(domChild);
+        if (i % 5 === 0) domParent.innerHTML += '<div class="break"></div>';
+    }
 }
-
-//var parentDiv = document.querySelector(".list-group");
-//parentDiv.insertAdjacentHTML("beforeend", "<div class='list-group-item col-xs-6 col-md-4'>ZZZZZ</div>");
-
 document.getElementById("myForm").onsubmit = function () {
     runBishBosh(document.getElementById("inputLoop").value, document.getElementById("inputFirstDiv").value,
         document.getElementById("inputSecondDiv").value);
